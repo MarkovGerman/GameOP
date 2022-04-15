50219fc2383ccdea39f2339b36565879a301354a
MarkovGerman <German.Markov@urfu.me>
2022-04-15 23:03:18 +0500

    Good lamps

--- Assets/Scripts/LightBehaviour.cs
+++ Assets/Scripts/LightBehaviour.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class LightBehaviour : MonoBehaviour
 {
     public float offset = -90;
+    public float range = 40;
+    public GameObject lamp;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(diff.x, diff.y) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, angle + offset);
+        Vector3 dif = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        // float angle = Mathf.Atan2(diff.x, diff.y) * Mathf.Rad2Deg;
+        // var vector = Quaternion.Euler(Vector3.Normalize(diff) * 40);
+        float angle = Mathf.Atan2(dif.x, -dif.y) * Mathf.Rad2Deg;
+        var diff = Vector3.Normalize(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position) * range;
+        transform.rotation = Quaternion.Euler(-diff.y, diff.x, angle + offset);
     }
 }
